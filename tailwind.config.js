bdd0c17950c90f5a778525025b6e8bfaf7c60f50
irina0516 <irina880011@gmail.com>
2022-11-23 00:34:08 +0900

    fix: marquee

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {
-        animation: {
-          'spin-slower': 'spin 3s linear infinite',
-          'spin-slower-reserve': "reverse spin 3s linear infinite",
-          marquee: 'marquee 100s linear infinite',
-        },
-        keyframes: {
-          marquee: {
-            '0%': { transform: 'translateY(0%)' },
-            '100%': { transform: 'translateY(-100%)' },
-          }
-        },
-    
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'spin-slower': 'spin 3s linear infinite',
+        'spin-slower-reserve': "reverse spin 3s linear infinite",
+        marquee: 'marquee var(--marquee-duration) linear infinite',
       },
+      keyframes: {
+        marquee: {
+          '100%': { transform: 'translateY(-50%)' },
+        }
+      },
+  
     },
-    plugins: [],
+  },
+  plugins: [],
 }
