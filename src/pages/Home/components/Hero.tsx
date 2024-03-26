d8a05b402422f3afb0a454c9f1758270c8dd7d58
maria <irina880011@gmail.com>
2024-03-26 12:44:09 +0800

    fix url

--- src/pages/Home/components/Hero.tsx
+++ src/pages/Home/components/Hero.tsx
@@ -359,7 +359,7 @@ export function Hero() {
             <div className="mt-8 flex flex-wrap gap-x-6 gap-y-4">
               <AppStoreLink />
               <Button
-                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                href="https://www.youtube.com"
                 variant="outline"
               >
                 <PlayIcon className="h-6 w-6 flex-none" />
